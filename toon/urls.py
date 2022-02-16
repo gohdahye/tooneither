6764798213c9f5a5d631e979262380bb4c3bc11c
gohdahye <kdhnate222@nate.com>
2022-02-16 17:32:55 +0900

    monday page 제작중

--- toon/urls.py
+++ toon/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('mon/', views.mon, name="mon" ),
     path('', views.home, name="home" ),
 ]
--- toon/views.py
+++ toon/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
     return render(
         request,
         'toon/home.html'
+    )
+
+def mon(request):
+    return render(
+        request,
+        'toon/mon.html'
     )
