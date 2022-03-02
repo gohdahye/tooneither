bf9295b15c6ab46fe47bb52b274a0a1d60c83f84
gohdahye <kdhnate222@nate.com>
2022-03-03 01:06:33 +0900

    create view

--- toon/urls.py
+++ toon/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('create_toon/', views.ToonCreate.as_view(), name="create"),
     path('<str:slug>/', views.days_page),
     path('<str:slug>/<int:pk>/', views.ToonDetail.as_view()),
     path('', views.ToonList.as_view(), name="home"),
